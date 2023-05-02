a800af24271ff5e7a97581fcf72bf7f8a6e7df25
Konrad Brożyna <kondzio3002@gmail.com>
2023-05-02 17:47:43 +0200

    Add endpoint getById for authors

--- src/authors/authors.controller.ts
+++ src/authors/authors.controller.ts
@@ -1,4 +1,10 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  ParseUUIDPipe,
+  NotFoundException
+} from '@nestjs/common';
 import { AuthorsService } from './authors.service';
 
 @Controller('authors')
@@ -8,5 +14,12 @@ export class AuthorsController {
   @Get('/')
   getAll() {
     return this.authorsService.getAll();
+  }
+
+  @Get('/:id')
+  async getById(@Param('id', new ParseUUIDPipe()) id: string) {
+    const author = await this.authorsService.getById(id);
+    if (!author) throw new NotFoundException('Author not found');
+    return author;
   }
 }
