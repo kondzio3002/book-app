9eec1d672a9309f3da0c9817f625338c5e84e9fc
Konrad Brożyna <kondzio3002@gmail.com>
2023-05-03 17:08:31 +0200

    Add endpoints for users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, NotFoundException, Param, ParseUUIDPipe, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
+
+  @Get('/')
+  getAll(): any {
+    return this.usersService.getAll();
+  }
+
+  @Get('/:id')
+  async getById(@Param('id', new ParseUUIDPipe()) id: string) {
+    const user = await this.usersService.getById(id);
+    if (!user) throw new NotFoundException('User not found');
+    return user;
+  }
 }
