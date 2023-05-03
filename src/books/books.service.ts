98a738f0a2a400bd035a22caceb8dc504bd07703
Konrad Brożyna <kondzio3002@gmail.com>
2023-05-03 11:07:05 +0200

    Add endpoint getAll for books

--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -1,7 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/shared/services/prisma.service';
+import { Book } from '@prisma/client';
 
 @Injectable()
 export class BooksService {
   constructor(private prismaService: PrismaService) {}
+
+  public getAll(): Promise<Book[]> {
+    return this.prismaService.book.findMany({
+      include: { author: true },
+    });
+  }
 }
